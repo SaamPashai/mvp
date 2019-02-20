75e58bb7eb38d3838daff6d09fa0bf48cdc3201b
Saam Pashai <doubleapashai@gmail.com>
2019-02-19 20:26:56 -0800

    added list

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ d3.csv("data/dummyData.csv").then(function (data) {
     console.log(dummybase);
     
 });
+
+
+
+$("#tableMenu a").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    var selText = $(this).text();
+    $("#tableButton").text(selText);
+    console.log(selText);
+    $('table#myTable tr#addition').remove();
+    if(selText === "Ballard"){
+        $('#myTable tr:last').after('<tr id="addition"><th>Restock Computers</th></tr><tr id="addition"><th>Finish Catalog</th></tr><tr id="addition"><th>Set up NAT</th></tr><tr id="addition"><th>Train new Employees</th></tr>');
+    }
+    if(selText === "Garfield"){
+        $('#myTable tr:last').after('<tr id="addition"><th>Reboot Network</th></tr><tr id="addition"><th>Remove old data</th></tr id="addition"><tr id="addition"><th>Write Scripts</th></tr>');
+    }
+    if(selText === "Franklin"){
+        $('#myTable tr:last').after('<tr id="addition"><th>Fix Labtops</th></tr><tr id="addition"><th>Provide new data</th></tr>');
+    }
+    if(selText === "Lincoln"){
+        $('#myTable tr:last').after('<tr id="addition"><th>Open new servers</th></tr>');
+    }
+});
