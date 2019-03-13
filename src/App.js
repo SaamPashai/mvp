fd1f74f376d9910e40687fb8c3bea30a19a8b443
Benny Nguyen <benny.nguyenx@gmail.com>
2019-03-13 14:49:21 -0700

    Start ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router, Route} from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
-
-import { Homepage } from './Homepage';
-
+import { Homepage } from './Homepage.js';
+//import { Login } from './Login.js'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <React.Fragment>
-            <Route exact path="/" component={Homepage} />
-          </React.Fragment>
-        </Router>
+        <Switch>
+          {/* <Route exact path="/" render={}/> */}
+          <Route exact path="/" component={Homepage} />
+        </Switch>
       </div>
     );
   }
