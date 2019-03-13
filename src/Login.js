889878d68fb1afa77f143fc84106471e398fd4a3
Saam Pashai <doubleapashai@gmail.com>
2019-03-13 15:03:43 -0700

    login functioning

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+
+
 export class Login extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome</h1>
+                <h1 className="text-center">Welcome</h1>
+                <div className="text-center form-inline">
+                    <form>
+                        <label>
+                            Username:
+                  <input type="text" name="name" />
+                        </label>
+                        <br></br>
+                        <label>
+                            Password:
+                  <input type="password" name="password" />
+                        </label>
+                        <br></br>
+                        <Link to ={"/home"} className="btn btn-primary">Submit</Link>
+                    </form>
+                </div>
             </div>
         )
     }
