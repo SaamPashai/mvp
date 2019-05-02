02e13152ea3a91cceec035ac6371e76d9123de09
Benny Nguyen <benny.nguyenx@gmail.com>
2019-05-02 16:20:33 -0700

    Add info button

--- src/components/dashboard/SchoolView/SchoolView.js
+++ src/components/dashboard/SchoolView/SchoolView.js
@@ -18,10 +18,12 @@ export class SchoolView extends Component {
     this.state = {
       usersSchools: [], // schools to be populated based on FireBase user
       schoolName: '',
-      modal: false
+      modal: false,
+      infoModal: false
     };
 
     this.toggle = this.toggle.bind(this);
+    this.toggleInfo = this.toggleInfo.bind(this);
   }
 
   componentDidMount() {
@@ -34,6 +36,18 @@ export class SchoolView extends Component {
         return state;
       })
     })
+  }
+
+  toggleInfo = (event) => {
+    let schoolName = '';
+    if (typeof(event) === 'string') {
+      schoolName = event;
+    }
+
+    this.setState(prevState => ({
+      infoModal: !prevState.infoModal,
+      schoolName
+    }));
   }
 
   // toggles whether or not modal is open
@@ -59,40 +73,67 @@ export class SchoolView extends Component {
 
   render() {
     const closeBtn = <button className="close" onClick={this.toggle}>&times;</button>
+    const infoCloseBtn = <button className="close" onClick={this.toggleInfo}>&times;</button>;
 
     // Making school cards
     let schoolCards = [];
+    let schoolMetaData = []; // storing metadata to get school properties from json file
     if (this.state.usersSchools !== null) {
-
       let usersSchools = Object.keys(this.state.usersSchools);
-      let schoolMetaData = []; // storing metadata to get school properties from json file
       json.forEach(entry => {
         if (usersSchools.includes(entry.name)) {
           schoolMetaData[entry.name] = entry;
         }
       });
-
       usersSchools.forEach(school => {
         schoolCards.push(
           <div>
-            
-              <Card className="schoolCard">
-                <CardBody>
-                  <CardTitle>{school}</CardTitle>
-                  <Button color="warning">Information</Button>
-                  <Button>
-                  <Link color="danger" to="/tasks" 
-                    onClick={(thisSchool) => this.props.getSchoolNameCallback(school)}>
-                    Tasks
-                  </Link>
-                  </Button>
-                </CardBody>
-              </Card>
+            <Card className="schoolCard">
+              <CardBody>
+                <CardTitle>{school}</CardTitle>
+                <Button color="warning" onClick={(thisSchool) => this.toggleInfo(school)}>Information</Button>
+                <Button>
+                <Link color="danger" to="/tasks" 
+                  onClick={(thisSchool) => this.props.getSchoolNameCallback(school)}>
+                  Tasks
+                </Link>
+                </Button>
+              </CardBody>
+            </Card>
           </div>
         );
       });
     }
 
+    // Modal for information on selected school
+    let infoModal = <div></div>;
+    if (this.state.schoolName !== '') {
+      infoModal =     
+      <div>
+        <Modal isOpen={this.state.infoModal} toggle={this.toggleInfo} className={this.props.className}>
+          <ModalHeader toggle={this.toggleInfo} close={infoCloseBtn}>School Information</ModalHeader>
+          <ModalBody>
+            <strong>Cluster:</strong> {schoolMetaData[this.state.schoolName].u_cluster}
+            <br/>
+            <strong>TS Specialist:</strong> {schoolMetaData[this.state.schoolName].u_ts_specialist}
+            <br/>
+            <strong>IT Specialist:</strong> {schoolMetaData[this.state.schoolName].u_it_specialist}
+            <br/>
+            <strong>School Address:</strong> {schoolMetaData[this.state.schoolName].u_address}
+            <br/>
+            <strong>School Phone Number:</strong> {schoolMetaData[this.state.schoolName].u_phone_number}
+            <br/>
+            <strong>Site Code:</strong> {schoolMetaData[this.state.schoolName].u_site_code}
+            <br/>
+            <strong>School Principal:</strong> {schoolMetaData[this.state.schoolName].u_principal}
+            <br/>
+            <strong>School Secretary:</strong> {schoolMetaData[this.state.schoolName].u_secretary}
+          </ModalBody>
+          <ModalFooter>
+          </ModalFooter>
+        </Modal>
+      </div>
+    }
     // getting list of school names to suggest during user input
     let schools = []
     json.forEach(entry => {
@@ -104,6 +145,7 @@ export class SchoolView extends Component {
         <div>
           <Button color="danger" onClick={this.toggle}>Add School</Button>
         </div>
+
         {/* Modal to add school. Opens and closes */}
         <Modal centered={true} isOpen={this.state.modal} toggle={this.toggle}>
         <ModalHeader toggle={this.toggle} close={closeBtn}>Add School</ModalHeader>
@@ -121,6 +163,9 @@ export class SchoolView extends Component {
           <Button color="secondary" onClick={this.toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
+
+      {/* Modal for information on school */}
+      {infoModal}
 
       {/* School cards */}
       <div className="container">
