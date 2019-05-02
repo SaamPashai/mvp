35b05ec731e214c70d08d20013cc5305b04fd6a8
Benny Nguyen <benny.nguyenx@gmail.com>
2019-05-01 17:54:46 -0700

    Fix route of logout button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase/app';
 import { SignUpForm } from './components/signup/SignUpForm.js'
 import { Spinner } from 'reactstrap';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
 // importing components
 import { Header } from './components/dashboard/Header.js';
@@ -139,7 +139,7 @@ export default class App extends Component {
 						/>
 					</Switch>
 					<button className="btn btn-warning" onClick={this.handleSignOut}>
-						Log Out {this.state.user.email}
+						<Link to="/">Log Out {this.state.user.email}</Link>
 					</button>
 				</div>
       );
