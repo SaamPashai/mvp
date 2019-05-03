db35ebe880f2ee304ff550298ec0159c028b4e40
Benny Nguyen <benny.nguyenx@gmail.com>
2019-05-03 04:28:22 -0700

    Add style

--- src/components/dashboard/TaskView/TaskView.js
+++ src/components/dashboard/TaskView/TaskView.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 
+import { Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 // importing components
 import { TaskCreationModal } from './TaskCreationModal.js';
 import { TaskList } from './TaskList.js'
+
+// importing CSS
+import './TaskView.css';
+
 export class TaskView extends Component {
-    render() {
-        return (
-            <div>
-                <TaskCreationModal currentUser={this.props.currentUser} schoolName={this.props.schoolName}/>
-                <TaskList currentUser={this.props.currentUser} schoolName={this.props.schoolName}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Button color="danger">
+          <Link to="/">Back</Link>
+        </Button>
+        <TaskCreationModal currentUser={this.props.currentUser} schoolName={this.props.schoolName}/>
+        <TaskList currentUser={this.props.currentUser} schoolName={this.props.schoolName}/>
+      </div>
+    )
+  }
 }
